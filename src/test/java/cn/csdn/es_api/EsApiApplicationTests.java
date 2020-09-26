3adcab3e73f81e064937df44205984ade1ecd37f
deng <1>
2020-09-26 16:17:05 +0800

    es搭建

---
+++ src/test/java/cn/csdn/es_api/EsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.csdn.es_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
